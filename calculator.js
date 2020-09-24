e525443d4ad2f0d5b3dbccbc75ac1b66ebe4e47c
mmrg98 <mariam.elgaria98@gmail.com>
2020-09-24 15:15:39 +0300

    small calculator

---
+++ calculator.js
@@ -0,0 +1,15 @@
+let n1 = 6
+let n2 = 3
+let op = "*"
+if (op == "+") {
+    console.log(`${n1} + ${n2} = ${n1+n2}`)
+}
+else if (op == "-"){
+    console.log(`${n1} - ${n2} = ${n1-n2}`)
+}
+else if (op == "/"){
+    console.log(`${n1} / ${n2} = ${n1/n2}`)
+}
+else if (op == "*"){
+    console.log(`${n1} * ${n2} = ${n1*n2}`)
+}
