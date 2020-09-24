93f31be8cbcd8f7c53cc6ee8019a3f37d0b1c97b
mmrg98 <mariam.elgaria98@gmail.com>
2020-09-24 15:19:38 +0300

    commit mes

---
+++ story.js
@@ -0,0 +1,5 @@
+let name = "Mariam"
+let age = 21
+let hobby = "Cooking"
+console.log(`Mr./Mrs. ${name}, in 33 years, you will be ${age + 33} years old.
+ By then you'll have decades of ${hobby} practice!`)
